bd24679fab01e22251c8b7e5507b3261073411d4
Abe Stone <abestone@ix.netcom.com>
2015-06-08 10:30:14 -0700

    Fixed (I hope) a bug that would crash a dedicated server (channel names are max 20 characters)

--- src/main/java/com/abocalypse/constructionflower/constructionflower.java
+++ src/main/java/com/abocalypse/constructionflower/constructionflower.java
@@ -4,13 +4,13 @@ import java.io.File;
 
 import net.minecraftforge.common.MinecraftForge;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
 import cpw.mods.fml.common.network.simpleimpl.SimpleNetworkWrapper;
@@ -25,7 +25,6 @@ import com.abocalypse.constructionflower.lib.ConfigHandler;
 import com.abocalypse.constructionflower.lib.Constants;
 import com.abocalypse.constructionflower.network.LoadPlanMessage;
 import com.abocalypse.constructionflower.network.LoadedPlansMessage;
-import com.abocalypse.constructionflower.network.SpawnedOntoBlocksMessage;
 import com.abocalypse.constructionflower.world.ConstructionFlowerWorldGen;
 
 @Mod(modid = Constants.MODID, name = Constants.MODNAME, version = Constants.VERSION)
@@ -37,6 +36,7 @@ public class ConstructionFlower {
 	public SimpleNetworkWrapper network;
 	
     // Says where the client and server 'proxy' code is loaded.
+	// See http://greyminecraftcoder.blogspot.com.au/2013/11/how-forge-starts-up-your-code.html
     @SidedProxy(clientSide="com.abocalypse.constructionflower.client.ClientProxy",
 	    serverSide="com.abocalypse.constructionflower.CommonProxy")
     public static CommonProxy proxy;
@@ -60,18 +60,30 @@ public class ConstructionFlower {
     	
     	GameRegistry.registerWorldGenerator(new ConstructionFlowerWorldGen(), 20);
     	
-     	network = new SimpleNetworkWrapper(Constants.MODID + "_Channel");
+    	// the register of plans (WorldPlanRegistry) is kept only on the
+    	// server, whereas gui's are opened only on the client. So both of
+    	// the commands, cfload and cfmanage, need to send messages back and
+    	// forth. 
+    	//
+    	// The two message handlers on the server side (for learning
+    	// what the player did with the gui) are registered here; the two
+    	// for the client side (for tellin the gui what to display) are
+    	// registered in the client proxy.
+    	//
+    	// Note the channel name can't be more than 20 characters or the
+    	// dedicated server will crash.
+    	// http://www.minecraftforum.net/forums/mapping-and-modding/minecraft-mods/modification-development/2171523-solved-the-received-string-length-is-longer-than?comment=11
+    	network = new SimpleNetworkWrapper(StringUtils.right(Constants.MODID, 20));
      	network.registerMessage(LoadPlanMessage.Handler.class, LoadPlanMessage.class, 2, Side.SERVER);
     	network.registerMessage(LoadedPlansMessage.ServerHandler.class, LoadedPlansMessage.class, 5, Side.SERVER);
+
     	proxy.init();
     }
 
-    @Mod.EventHandler
-    public void postInit(FMLPostInitializationEvent event) {
-
-    }
-
-    
+    // I believe this is primarily for the purpose of integrating
+    // with other mods? Nothing to put here as of now.
+    //@Mod.EventHandler
+    //public void postInit(FMLPostInitializationEvent event) {}
 
     @Mod.EventHandler
     public void serverLoad(FMLServerStartingEvent event)
